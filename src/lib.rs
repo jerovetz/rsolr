e1e284bf927a6f62cb22b1d428b17cf02df21f68
jurijuri <jerovetz@comlions.net>
2023-04-12 11:30:20 +0200

    json handling, query_all

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,24 @@
 pub struct Client {
-
+    host: String,
+    collection: String
 }
 
 impl Client {
     pub fn hello() -> &'static str
     {
         "Hello"
+    }
+
+    pub fn query_all(&self) -> serde_json::Result<serde_json::Value> {
+        serde_json::from_str(r#"{}"#)
+    }
+
+    pub fn new(url : &str, collection : &str) -> Client {
+        let host = String::from(url);
+        let collection = String::from(collection);
+        Client {
+            host,
+            collection
+        }
     }
 }
--- tests/client_test.rs
+++ tests/client_test.rs
@@ -1,7 +1,18 @@
 use rsc::Client;
 
 #[test]
-fn test_ping() {
+fn test_hello() {
     let s = Client::hello();
     assert_eq!(s, "Hello")
+}
+
+#[test]
+fn test_query_all() {
+    let data = r#"{}"#;
+    let expected_documents : serde_json::Value = serde_json::from_str(data).unwrap();
+    let host = "http://localhost:8983";
+    let collection = "default";
+    let client = Client::new(host, collection);
+    let result = client.query_all();
+    assert_eq!(result.unwrap(), expected_documents);
 }
