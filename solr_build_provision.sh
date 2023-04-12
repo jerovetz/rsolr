5cfb511afc71f7a34174912469523407c21ecf2f
jurijuri <jerovetz@comlions.net>
2023-04-12 15:55:59 +0200

    remove -it

--- solr_build_provision.sh
+++ solr_build_provision.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker exec -it rsc_solr sh -c 'solr delete -c default; solr create -c default'
+docker exec rsc_solr sh -c 'solr delete -c default; solr create -c default'
 curl --location --request POST 'http://127.0.0.1:8983/api/cores/default/config' \
 --header 'Content-Type: application/json' \
 --data-raw '{
