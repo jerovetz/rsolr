e6f2a27efaa3a118b6a6235667d308d4acecc51a
jurijuri <jerovetz@comlions.net>
2023-04-20 12:08:39 +0200

    test message

--- src/lib.rs
+++ src/lib.rs
@@ -21,24 +21,28 @@ impl<'a> Client<'a> {
 
         let solr_result =  http_client
             .get(&format!("{}/solr/{}/select?q={}", &self.host, &self.collection, &query));
+
         let raw_response = match solr_result {
             Ok(response) => response,
             Err(e) => return Err(RSCError { source: Some(Box::new(e)), status: None, message: None }),
         };
+
         let response_status = raw_response.status();
+
         if response_status == StatusCode::NOT_FOUND {
             return Err(RSCError { source: None, status: Some(response_status), message: None })
         };
 
         if response_status != StatusCode::OK {
-            let message_string = match raw_response.json::<Value>() {
+            let body_text = raw_response.text().unwrap();
+            let message_string = match serde_json::from_str::<Value>(&body_text) {
                 Ok(r) => r["error"]["msg"].to_string(),
                 Err(e) => {
                     return Err(
                         RSCError {
                             source: Some(Box::new(e)),
                             status: Some(response_status),
-                            message: Some(String::from("Unparseable body."))
+                            message: Some(body_text)
                         })
                 }
             };
@@ -62,9 +66,25 @@ impl<'a> Client<'a> {
 mod tests {
     use super::*;
 
+    use std::sync::{Mutex, MutexGuard};
+    use mockall::lazy_static;
+
+    lazy_static! {
+        static ref MTX: Mutex<()> = Mutex::new(());
+    }
+
+    fn get_lock(m: &'static Mutex<()>) -> MutexGuard<'static, ()> {
+        match m.lock() {
+            Ok(guard) => guard,
+            Err(poisoned) => poisoned.into_inner(),
+        }
+    }
+
     #[test]
     fn test_query_responds_rsc_error_with_other_problem_if_dunno() {
+        let _m = get_lock(&MTX);
         let ctx = HttpClient::new_context();
+
         ctx.expect().returning(|| {
             let mut mock = HttpClient::default();
             mock.expect_get().returning(|_| Ok(reqwest::blocking::Response::from(http::response::Builder::new().status(500).body(r#"{"error": {"code": 500, "msg": "okapi"}}"#).unwrap())));
@@ -77,13 +97,16 @@ mod tests {
         assert!(result.is_err());
         let error = result.err().expect("No Error");
         assert_eq!(error.status().unwrap(), StatusCode::INTERNAL_SERVER_ERROR);
-
+        assert_eq!(error.message().unwrap(), "okapi");
         assert!(matches!(error.kind(), error::ErrorKind::Other));
+        ctx.checkpoint();
     }
 
     #[test]
     fn test_query_responds_rsc_error_with_raw_text_body_and_status_code_if_no_standard_message() {
+        let _m = get_lock(&MTX);
         let ctx = HttpClient::new_context();
+        ctx.checkpoint();
         ctx.expect().returning(|| {
             let mut mock = HttpClient::default();
             mock.expect_get().returning(|_| Ok(reqwest::blocking::Response::from(http::response::Builder::new().status(500).body(r#"some unparseable thing"#).unwrap())));
@@ -96,7 +119,7 @@ mod tests {
         assert!(result.is_err());
         let error = result.err().expect("No Error");
         assert_eq!(error.status().unwrap(), StatusCode::INTERNAL_SERVER_ERROR);
-
+        assert_eq!(error.message().unwrap(), "some unparseable thing");
         assert!(matches!(error.kind(), error::ErrorKind::Other));
     }
 
