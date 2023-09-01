804bc8291bdfa387971ece0c5dc70d9899f3cbb0
jurijuri <jerovetz@comlions.net>
2023-09-01 13:40:22 +0200

    basic field value getter

--- src/facet_fields.rs
+++ src/facet_fields.rs
@@ -5,5 +5,36 @@ use serde_json::Value;
 #[derive(Clone, Debug, Deserialize)]
 pub struct FacetFields {
     #[serde(flatten)]
-    pub fields: Value
+    pub fields: Value,
+    field_value: String
+}
+
+impl FacetFields {
+
+    pub fn field(&mut self, value: String) -> &mut Self {
+        self.field_value = value;
+        self
+    }
+
+    pub fn get(&self, key: String) -> u64 {
+        let mut array_iter = self.fields[&self.field_value].as_array().unwrap().iter();
+        while array_iter.next().unwrap().as_str().unwrap() != key { }
+
+        array_iter.next().unwrap().as_u64().unwrap()
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::facet_fields::FacetFields;
+
+    #[test]
+    fn test_get_custom_field_count() {
+        let fields = serde_json::from_str(r#"{"field_value": ["val1", 123, "val2", 234] }"#).unwrap();
+        let mut facet_fields = FacetFields { fields, field_value: "".to_string() };
+
+        assert_eq!(facet_fields.field("field_value".to_string()).get("val1".to_string()), 123)
+    }
+
 }
