b7c8f622f6822c0b9ccaf8cd5ea48754ed1f03dd
jurijuri <jerovetz@comlions.net>
2023-09-08 11:42:21 +0200

    remove unnecesary provision step, enable stream body on build instance

--- solr_provision.sh
+++ solr_provision.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
 docker exec rsc_solr sh -c 'solr delete -c default; solr create -c default'
-curl --location --request POST 'http://127.0.0.1:8983/api/cores/default/config' \
---header 'Content-Type: application/json' \
---data-raw '{
-    "set-property": [{
-            "requestDispatcher.requestParsers.enableRemoteStreaming": true
-        },
-        {
-            "requestDispatcher.requestParsers.enableStreamBody": true
-        }
-    ]
-}'
